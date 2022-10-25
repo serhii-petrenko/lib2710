d55bc6898d3423e73ff6e7ff90dbd414a076c8af
Serhii Petrenko <petrenko.zd@gmail.com>
2022-10-25 22:30:04 +0300

    init commit

---
+++ src/main/java/ua/javaclub/CalculatorService.java
@@ -0,0 +1,13 @@
+package ua.javaclub;
+
+public class CalculatorService {
+
+    public Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public Integer diff(Integer a, Integer b) {
+        return a - b;
+    }
+
+}
